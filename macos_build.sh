f453c015401f590bcac1061d7580ea0f154158e5
Patrick Cleavelin <patrick@uptrainsoftware.com>
2019-12-04 16:08:23 -0800

    fix executable flags of macOS build scripts

--- macos_build.sh
+++ macos_build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 gcc -c src/macOS/macOS_pdl.m -o $OUT_DIR/macOS_pdl.obj -fobjc-arc
 gcc -c src/macOS/Delegates.m -o $OUT_DIR/macOS_delegates.obj -fobjc-arc
 ar rcs $OUT_DIR/libpdl.a $OUT_DIR/macOS_delegates.obj $OUT_DIR/macOS_pdl.obj
